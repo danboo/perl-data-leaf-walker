db6ac5e8296ca1388a5c31c5b709ad2938faf61e
Dan Boorstein <danboo@danboo-desktop.(none)>
2010-01-02 14:47:08 -0500

    - moved to version 0.02

--- lib/Data/Leaf/Walker.pm
+++ lib/Data/Leaf/Walker.pm
@@ -9,11 +9,11 @@ Data::Leaf::Walker - Walk the leaves of arbitrarily deep nested data structures.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
